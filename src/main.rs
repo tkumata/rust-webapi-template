9c5ae0f3d2fa6578e13cce202fe1d737e27af8a6
Tomokatsu KUMATA <tkumata@gmail.com>
2023-10-08 12:03:03 +0000

    feat: #1 basic method.

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,26 @@ use axum::{
 #[tokio::main]
 async fn main() {
     // build our application with a single route
+    // Todo: プラグイン的にとっかえひっかえができるようなカタチにする。
     let app = Router::new()
-    .route(
-        "/",
-        get(|| async {
-            "Hello, Rust World!"
-        })
-    )
-    .route(
-        "/foo",
-        get(|| async {
-            "Hello, foo World."
-        })
-    )
-    .route(
-        "/bar",
-        get(|| async {
-            "Hello, bar world."
-        })
-    );
+        .route(
+            "/",
+            get(|| async {
+                "Hello, Rust World! DocumentRoot."
+            })
+        )
+        .route(
+            "/foo",
+            get(|| async {
+                "Hello, Rust GET World! /foo"
+            })
+        )
+        .route(
+            "/post",
+            post(|| async {
+                "Hello, Rust POST World. /post"
+            })
+        );
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
