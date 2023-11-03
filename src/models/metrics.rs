d4a52121a4766b6ae3b50dd125ba8830de138565
tkumata <tkumata@gmail.com>
2023-11-03 15:12:29 +0900

    fix: #6 tmp variable names

--- src/models/metrics.rs
+++ src/models/metrics.rs
@@ -3,12 +3,18 @@ use sysinfo::{System, SystemExt};
 pub async fn get_load() -> String {
     let sys = System::new_all();
     let load_avg = sys.load_average();
-    let s = [load_avg.one.to_string(), load_avg.five.to_string(), load_avg.fifteen.to_string()].join(",");
+    let s = [
+        load_avg.one.to_string(),
+        load_avg.five.to_string(),
+        load_avg.fifteen.to_string()
+    ].join(",");
+
     s
 }
 
 pub async fn get_mem() -> u64 {
     let sys = System::new_all();
-    let mem_usage = sys.used_memory() as u64;
-    mem_usage
+    let m = sys.used_memory() as u64;
+
+    m
 }
