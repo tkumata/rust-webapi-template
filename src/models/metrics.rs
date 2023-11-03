8680128322aa415d616e615b29b480656294fdb3
tkumata <tkumata@gmail.com>
2023-11-03 18:21:50 +0900

    refactor: #6 Change struct name

--- src/models/metrics.rs
+++ src/models/metrics.rs
@@ -2,28 +2,25 @@ use sysinfo::{System, SystemExt, DiskExt};
 
 pub async fn get_kernelname() -> Option<String> {
     let sys = System::new();
-    let name = sys.name();
 
-    name
+    sys.name()
 }
 
 pub async fn get_load() -> String {
     let sys = System::new_all();
     let load_avg = sys.load_average();
-    let s = [
+
+    [
         load_avg.one.to_string(),
         load_avg.five.to_string(),
         load_avg.fifteen.to_string()
-    ].join(",");
-
-    s
+    ].join(",")
 }
 
 pub async fn get_mem() -> u64 {
     let sys = System::new_all();
-    let m = sys.used_memory() as u64;
 
-    m
+    sys.used_memory() as u64
 }
 
 pub async fn get_storage() -> String {
