f2b33ecb705ef013b262c2423851ed678e899ea2
Tomokatsu Kumata <tkumata@gmail.com>
2024-11-13 14:02:38 +0000

    cleanup

--- src/controllers/convert.rs
+++ src/controllers/convert.rs
@@ -18,7 +18,7 @@ pub async fn rgb(
     Json(req_rgb): Json<RequestRgb>
 ) -> impl IntoResponse {
 
-    let converted: utils::utils::ConvertedRgb = utils::utils::to_rgb_hex(
+    let converted: utils::utility::ConvertedRgb = utils::utility::to_rgb_hex(
         req_rgb.r,
         req_rgb.g,
         req_rgb.b
@@ -38,7 +38,7 @@ pub async fn v4_prefix(
     Json(req_prefix): Json<RequestNetPrefix>
 ) -> impl IntoResponse {
 
-    let converted: std::net::IpAddr = utils::utils::to_subnetmask(req_prefix.bit_length);
+    let converted: std::net::IpAddr = utils::utility::to_subnetmask(req_prefix.bit_length);
     let subnetmask: String = converted.to_string();
 
     (StatusCode::OK, subnetmask)
